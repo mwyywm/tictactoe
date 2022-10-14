1c73c8aeb6882d4bc2d18f6c3d38a743242e6b9e
mwyywm <hasanof.rasmus@gmail.com>
2022-10-14 19:21:55 +0200

    feat: click function
    - Shows curr players turn and if they use X or O

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [tiles, setTiles] = useState([
+  const [currPlayer, setCurrPlayer] = useState<1 | 2>(1);
+  const [tiles, setTiles] = useState<number[][] | undefined[][]>([
     [undefined, undefined, undefined],
     [undefined, undefined, undefined],
     [undefined, undefined, undefined],
   ]); // 2d Array
   // player 1 = X
   // player 2 = O
+
+  function handleClick(currPlayer: 1 | 2, row: number, col: number) {
+    if (tiles[row][col] !== undefined) return; // Only allows a single click per tile.
+    currPlayer === 1 ? setCurrPlayer(2) : setCurrPlayer(1); // change current player every click
+
+    // Set the index in tiles 2D array to the players number
+    setTiles((prev: number[][] | undefined[][]) => {
+      prev[row][col] = currPlayer;
+      return prev;
+    });
+  }
   return (
     <div className="App">
+      <h1>Player {currPlayer} turn!</h1>
+      <h3>
+        Player {currPlayer} uses {currPlayer === 1 ? "X" : "O"}
+      </h3>
       {tiles.map((row, rowIndex) => (
         <div className="container" key={rowIndex}>
           {row.map((_, colIndex) => (
-            <div className="tile" key={`${rowIndex}${colIndex}`}>
-              <p>{rowIndex}</p>
-              <p>{colIndex}</p>
+            <div
+              className="tile"
+              key={`${rowIndex}${colIndex}`}
+              onClick={() => handleClick(currPlayer, rowIndex, colIndex)}
+            >
+              <p>
+                {tiles[rowIndex][colIndex] === 1
+                  ? "X"
+                  : tiles[rowIndex][colIndex] === 2
+                  ? "O"
+                  : undefined}
+              </p>
             </div>
           ))}
         </div>
