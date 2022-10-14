23159c3827cd4b32c85a72c93e847a12d8e3d9f6
mwyywm <hasanof.rasmus@gmail.com>
2022-10-14 21:19:09 +0200

    feat: check for winning player on turn

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [currPlayer, setCurrPlayer] = useState<1 | 2>(1);
-  const [tiles, setTiles] = useState<number[][] | undefined[][]>([
-    [undefined, undefined, undefined],
-    [undefined, undefined, undefined],
-    [undefined, undefined, undefined],
-  ]); // 2d Array
+  const [lastClicked, setLastClicked] = useState<number[]>([0, 0]); // [row, col]
+  const [tiles, setTiles] = useState<number[][]>([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]); // 2D Array
+  const [round, setRound] = useState(0);
+  const [winningPlayer, setWinningPlayer] = useState<number | false>(false);
   // player 1 = X
   // player 2 = O
+  // everytime tiles changes we need to check if we have 3 in a row
 
   function handleClick(currPlayer: 1 | 2, row: number, col: number) {
-    if (tiles[row][col] !== undefined) return; // Only allows a single click per tile.
+    if (tiles[row][col] !== 0) return; // Only allows a single click per tile.
     currPlayer === 1 ? setCurrPlayer(2) : setCurrPlayer(1); // change current player every click
-
     // Set the index in tiles 2D array to the players number
-    setTiles((prev: number[][] | undefined[][]) => {
+    setTiles((prev: number[][]) => {
       prev[row][col] = currPlayer;
       return prev;
     });
+    setLastClicked([row, col]);
+  }
+  function threeInARow(row: number, col: number): number | false {
+    // we check the last clicked tile to see if we have 3 in a row.
+    // check row
+    if (
+      tiles[row].every((elem: number) => elem > 0 && elem === tiles[row][col])
+    ) {
+      return tiles[row][0]; // winning player
+    }
+    // check vertically
+    if (tiles.every((arr) => arr[col] > 0 && arr[col] === tiles[row][col])) {
+      return tiles[row][col]; // winning player
+    }
+    // temp arrays for checking diagonals 00 11 22 and 20 11 02
+    let temp1: number[] = [];
+    let temp2: number[] = [];
+    for (let i = 0; i < 3; i++) {
+      // inserting data into temp arrays
+      temp1.push(tiles[i][i]);
+      temp2.push(tiles[i][2 - i]);
+    }
+    // Diagonal check
+    if (temp1.every((elem) => temp1[0] === elem && elem > 0)) {
+      return temp1[0]; // winning player
+    } else if (temp2.every((elem) => temp1[0] === elem && elem > 0)) {
+      return temp2[0];
+    }
+    // No match was made returns false
+    return false;
+  }
+  useEffect(() => {
+    if (!!threeInARow(lastClicked[0], lastClicked[1])) {
+      setWinningPlayer(threeInARow(lastClicked[0], lastClicked[1]));
+    }
+  }, [currPlayer]);
+  if (!!winningPlayer) {
+    return (
+      <div>
+        <h1>Player {winningPlayer} Won the game!</h1>
+      </div>
+    );
   }
   return (
     <div className="App">
